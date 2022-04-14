b0b8a684cb40d32af4519b3030e6df9780effbc4
bellzar7 <belelya.nazar1999@gmail.com>
2022-04-14 23:03:45 +0300

    JS HW1 in progress

---
+++ JS/hw1/hw1.js
@@ -0,0 +1,118 @@
+/*
+1
+let a = 'hello';
+console.log(a);
+alert(a);
+document.write(a);
+
+let b = 'owu'
+console.log(b);
+alert(b);
+document.write(b);
+
+let c = 'com';
+console.log(c);
+alert(c);
+document.write(c);
+
+let d = 'ua';
+console.log(d);
+alert(d);
+document.write(d);
+
+let e = 1;
+console.log(e);
+alert(e);
+document.write(e);
+
+let f = 10;
+console.log(f);
+alert(f);
+document.write(f);
+
+let g = -999;
+console.log(g);
+alert(g);
+document.write(g);
+
+let h = 123;
+console.log(h);
+alert(h);
+document.write(h);
+
+let i = 3.14;
+console.log(i);
+alert(i);
+document.write(i);
+
+let j = 2.7;
+console.log(j);
+alert(j);
+document.write(j);
+
+let k = 16;
+console.log(k);
+alert(k);
+document.write(k);
+
+let l = 5 < 6;
+console.log(l);
+alert(l);
+document.write(l);
+
+let m = 5 > 6;
+console.log(m);
+alert(m);
+document.write(m);
+*/
+/*
+
+2
+let book = {
+    name: 'namebook',
+    pages: 328,
+    ganre: 'horror'
+}
+
+console.log(book);*/
+
+/*
+3
+let book = {
+    name: 'atlant',
+    pages: 1200,
+    ganre: 'literature',
+    autor: ['roy', 'jones', 'jr']
+}
+
+console.log(book.autor[0]);
+*/
+
+/*
+4
+let firstName = 'Nazariy';
+let lastName = 'Belelya';
+let middleName = 'Ivanovych';
+
+let person = `${firstName} ${middleName} ${lastName}`;
+console.log(person);
+*/
+
+let person = {
+    firstName: 'Nazariy',
+    lastName: 'Belelya',
+    age: '22 years old'
+}
+
+
+// let firstName = 'Nazariy';
+// let lastName = 'Belelya';
+// let age = '22 years old';
+
+
+let vv = prompt('what is your first name?');
+let vv = prompt('what is your last name?');
+let vv = prompt('how old are you?');
+
+console.log(person[vv])
+
