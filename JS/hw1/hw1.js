b2f4cf2a24c42728e68f0c365efd762684cca9ca
bellzar7 <belelya.nazar1999@gmail.com>
2022-04-15 19:16:25 +0300

    JS HW1 done

--- JS/hw1/hw1.js
+++ JS/hw1/hw1.js
@@ -98,21 +98,83 @@ let person = `${firstName} ${middleName} ${lastName}`;
 console.log(person);
 */
 
-let person = {
-    firstName: 'Nazariy',
-    lastName: 'Belelya',
-    age: '22 years old'
+/*
+5
+let name = prompt('what is your first name?');
+let sname = prompt('what is your last name?');
+let age = prompt('how old are you?');
+let person = `${name} ${sname} ${age}`
+let person = (name + sname + age);
+console.log(person);
+*/
+
+/*
+6
+let a = 100;
+console.log(typeof a);
+let b = '100';
+console.log(typeof b);
+let c = true;
+console.log(typeof c);
+*/
+
+/*7
+let book = [
+    {name:'name',pages:123,ganre:'ganre',autors:'autors'},
+    {name:'name1',pages:1234,ganre:'ganre1',autors:'autors1'},
+    {name:'name2',pages:12345,ganre:'ganre2',autors:'autors2'},
+];
+console.log(book[0]);
+console.log(book[1]);
+console.log(book[2]);*/
+
+// additional
+
+/*8
+let h = 23;
+let w = 10;
+let s = w * h;
+console.log(s);*/
+
+/*9
+let hC = 10;
+let dC = 4;
+let rC = dC / 2;
+let P = 3.14;
+let v = P*rC**rC*hC;
+console.log(v);*/
+
+/*10
+let n = 3;
+let m = 4;
+let k = Math.sqrt((Math.pow(n,2) + m**2));
+console.log(k);*/
+
+/*11
+console.log(5<6);
+console.log(5>6);
+console.log(5==6);
+console.log(5===6);
+console.log(10==10);
+console.log(10===10);
+console.log(10<10);
+console.log(10>10);
+console.log(10!=10);
+console.log(123==='123');
+console.log(123=='123');*/
+
+
+let a = 100;
+let b = 500;
+let c = "hello";
+let d = "okten";
+const x = 'constant value';
+{
+    console.log(a);
+    /*x = 'new value';*/ /*константу не можна змінювати*/
 }
+b = 'new value';  /*b вже визначене, не писати лет, а просто б=*/
 
+let result = 100 + 'y'; /*тут у зробити стрінгом або дати значення*/
 
-// let firstName = 'Nazariy';
-// let lastName = 'Belelya';
-// let age = '22 years old';
-
-
-let vv = prompt('what is your first name?');
-let vv = prompt('what is your last name?');
-let vv = prompt('how old are you?');
-
-console.log(person[vv])
-
+console.log(result); /*тут добавити log*/
