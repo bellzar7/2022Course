faa84a37a1539f6b57a5e36d3b0522d841bf1e23
bellzar7 <belelya.nazar1999@gmail.com>
2022-05-02 13:15:17 +0300

    JS HW4 additional1 done

--- JS/hw4/qwe.js
+++ JS/hw4/qwe.js
@@ -24,9 +24,12 @@ console.log(cyl(1,2));*/
 /*function arr(array) {
     for (let arrayElement of array) {
         console.log(arrayElement);
-    }
-}*/
 
+    }
+}
+
+let a = [1, 2, 3, 4, 5, 6]
+arr(a)*/
 // - створити функцію яка створює параграф з текстом. Текст задати через аргумент
 /*
 function text(text) {
@@ -58,17 +61,15 @@ ulcreator('qwe');*/
 ulcrea('qwasdasde', 3);*/
 
 // - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
-/*let users = [
-    {id: 1, name: 'Vasya', status: true},
-    {id: 2, name: 'olya', status: false},
-    {id: 3, name: 'maksim', status: true},
-    {id: 4, name: 'andrey', status: false},
-    {id: 5, name: 'valera', status: true}
-];
+/*let users = [1,'qweqwr',true,12,false,'qwerew'];
 function arr(arr) {
-    for (let arrElement of arr) {
-        console.log(arrElement);
-    }
+
+        document.write(`<ul>`);
+        for (const item of arr) {
+            document.write(`<li>${item}</li>`)
+        }
+        document.write(`</ul>`);
+
 }
 arr(users);*/
 
@@ -88,7 +89,7 @@ function arr(arr) {
 arr(users);*/
 
 // - створити функцію яка повертає найменьше число з масиву
-/*let arrs = [1, 2, 21, 2121, 11, 0.5];
+/*let arrs = [1, 2, 21, 2121, 11, 0.5, -3];
 function minnth(nth) {
     let nin = nth[0];
     for (const arr of nth) {
@@ -102,7 +103,7 @@ minnth(arrs);*/
 
 // - створити функцію яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад c->13
 /*
-let arr = [1,2,10];
+let arr = [1,2,10,4];
 function plus(nth) {
     let s = 0;
     for (const nthElement of nth) {
@@ -110,4 +111,173 @@ function plus(nth) {
     }
     return s;
 }
-console.log(plus(arr));*/
+console.log(plus(arr));
+*/
+
+/*
+------------------------------------------------------------------------------------------------------------------------
+                                           A D D I T I O N A L 1                                                        */
+
+//
+// - створити функцію яка приймає три числа та виводить найменьше. (Без Math.min!)
+/*function minnum(num1, num2, num3) {
+    if (num1<num2 && num1<num3){return num1}
+    else if (num2<num1 && num2<num3){return num2}
+    else if (num3<num1 && num3<num2){return num3}
+    else {return NaN}
+}
+
+console.log(minnum(11, 3, 2));*/
+
+// - створити функцію яка приймає три числа та виводить найбільше. (Без Math.max!)
+/*function minnum(num1, num2, num3) {
+    if (num1>num2 && num1>num3){return num1}
+    else if (num2>num1 && num2>num3){return num2}
+    else if (num3>num1 && num3>num2){return num3}
+    else {return NaN}
+}
+
+console.log(minnum(7, 11, 2));*/
+
+// - створити функцію яка повертає найбільше число з масиву
+/*function minnum(arr) {
+    let s = arr[0]
+    for (const item of arr) {
+        if (item<s){
+            s=item
+        }
+    }
+    return s;
+}
+a= [10,3,1]
+console.log(minnum(a))*/
+
+// - створити функцію яка приймає масив чисел та повертає середнє арифметичне його значень.
+/*function serznach(arrnum){
+    let s = 0
+    for (const arrnumElement of arrnum) {
+        s+=arrnumElement
+    }
+    return (s / arrnum.length)
+}
+let a = [1,2,3,4,5,6,7]
+console.log(serznach(a));*/
+
+// - створити функцію яка приймає будь-яку кількість чисел, повертає найменьше, а виводить найбільше (Math використовувати заборонено);
+/*function f(...num) {
+    let s = num[0]
+    let a = num[0]
+    for (const numElement of num) {
+        if (numElement > a) {
+            a = numElement
+        }
+       else if (numElement < s) {
+            s = numElement
+        }
+    }
+    console.log(a);
+    return s;
+}
+
+ f(3, 7, 4, 2, 3);*/
+
+// - створити функцію яка заповнює масив рандомними числами
+// (цей код генерує рандомні числа в діапазоні від 0 до 100 - Math.round(Math.random()*100)) та виводить його.
+/*function f(...num) {
+   let arr = []
+    for (let i = 0; i < 10; i++) {
+        arr[i] = Math.round(Math.random()*100)
+    }
+    return arr
+}
+console.log(f());*/
+
+// - створити функцію яка заповнює масив рандомними числами в діапазоні від 0 до limit. limit - аргумент, який характеризує кінцеве значення діапазону.
+/*function rand(limit) {
+    let arr = []
+    for (let i = 0; i < 10; i++) {
+        arr[i] = Math.round(Math.random()*limit)
+    }
+    return arr
+}
+console.log(rand(27));*/
+
+// - Функція приймає масив та робить з нього новий масив в зворотньому порядку. [1,2,3] -> [3, 2, 1].
+/*function f(arrs) {
+    let a = []
+    for (let i = arrs.length - 1; i >= 0; i--) {
+        a.push(arrs[i])
+    }
+    return a
+}
+let d = [1,2,3,4,5,6,7]
+console.log(f(d));*/
+//
+//
+//
+// - створити функцію, яка якщо приймає один аргумент, просто вивдоить його, якщо два аргументи - складає або конкатенує їх між собою.
+/*function f() {
+    let s = 0
+    if (arguments.length === 1) {
+        return arguments;
+    } else {
+        for (const argumentElement of arguments) {
+
+            s += argumentElement
+        }
+        return s
+    }
+}
+console.log(f(12 , 11, 12));*/
+//
+// - створити функцію  яка приймає два масиви та скаладає значення елементів з однаковими індексами  та повертає новий результуючий масив.
+//     EXAMPLE:
+// [1,2,3,4]
+//     [2,3,4,5]
+// результат
+//     [3,5,7,9]
+//
+/*function arrs(arr1,arr2) {
+    let narr = [];
+    for (let i = 0; i < arr1.length; i++) {
+        narr.push(arr1[i]+arr2[i])
+        }
+    return narr
+}
+
+let a = [1,2,3,4];
+let b = [2,3,4,5];
+console.log(arrs(a,b));*/
+
+// - Створити функцію яка приймає масив будь яких объектів, та повертає масив ключів всіх обєктів
+// EXAMPLE:
+//     [{name: 'Dima', age: 13}, {model: 'Camry'}]  ===> [ name, age, model ]
+
+/*function f(arr){
+    let a = [];
+    for (const item of arr) {
+        for (const itemKey in item) {
+            a.push(itemKey)
+        }
+    }
+    return a;
+}
+let b = [{name: 'Dima', age: 13}, {model: 'Camry'}];
+console.log(f(b));*/
+//
+//     - Створити функцію яка приймає масив будь яких объектів, та повертає масив значень всіх обєктів
+// EXAMPLE:
+//     [{name: 'Dima', age: 13}, {model: 'Camry'}]  ===> [ Dima, 13, Camry ]
+
+/*
+function f(arr) {
+    let a = [];
+    for (const item of arr) {
+        for (const itemKey in item) {
+            a.push(item[itemKey])
+        }
+    }
+    return a;
+}
+let b = [{name: 'Dima', age: 13}, {model: 'Camry'}];
+console.log(f(b));*/
